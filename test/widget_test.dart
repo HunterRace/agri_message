d59ee9b20181f1ab04683cecd68b3f0d8e72b70c
noelnobi <lqhung@gmail.com>
2019-09-13 12:47:29 +0700

    Add travis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,10 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:agri_message/main.dart';
 
 void main() {
-  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-    expect(find.text('Hello World!'), findsOneWidget);
-  });
 
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
 }
